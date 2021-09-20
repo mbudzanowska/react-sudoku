05093c1f92dcf49be16ff985b89dd4dcc9501cb1
Maria Budzanowska <budzanowska.maria@gmail.com>
2021-09-20 12:00:25 +0200

    add global styles using styled-components

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import { unregister } from './core'
+import { GlobalStyles } from './styles'
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyles />
     <div>Hello world!</div>
   </React.StrictMode>,
   document.getElementById('root')
