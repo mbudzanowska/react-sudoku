0a1bdfc543230919ca5db444033fa4975eff59e1
Maria Budzanowska <budzanowska.maria@gmail.com>
2021-09-20 12:13:28 +0200

    add theme as ThemeProvider from styled-components create theme object with colors, add theme typings, use themed colors in globalstyles

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { ThemeProvider } from 'styled-components'
 
 import { unregister } from './core'
-import { GlobalStyles } from './styles'
+import { GlobalStyles, theme } from './styles'
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyles />
-    <div>Hello world!</div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <div>Hello world!</div>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
---
+++ src/styles/theme.ts
@@ -0,0 +1,10 @@
+export default {
+  colors: {
+    background: 'radial-gradient(#282c34cc, #282c34)',
+    balck: '#282c34',
+    blue: '#a0e9fd',
+    lightBlue: '#caf3fe',
+    white: 'white',
+  },
+  transistion: '0.3s',
+}
