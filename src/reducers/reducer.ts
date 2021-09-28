94582e67a9135ad386911d022366255b40a88ba7
Maria Budzanowska <budzanowska.maria@gmail.com>
2021-09-28 11:14:00 +0200

    sudoku field selection functionality

--- src/reducers/reducer.ts
+++ src/reducers/reducer.ts
@@ -10,8 +10,16 @@ function reducer(state = initialState, action: AnyAction) {
   switch (action.type) {
     case types.CREATE_GRID:
       return {
+        ...state,
         grid: createFullGrid(),
       }
+
+    case types.SELECT_BLOCK:
+      return {
+        ...state,
+        selectedBlock: action.coords,
+      }
+
     default:
       return state
   }
