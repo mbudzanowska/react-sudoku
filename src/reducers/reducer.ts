b0e0240e8d69554621413c1afda5b18f5d9776ac
Maria Budzanowska <budzanowska.maria@gmail.com>
2021-09-28 13:23:53 +0200

    add function that creates Sudoku Puzzle with given difficulty

--- src/reducers/reducer.ts
+++ src/reducers/reducer.ts
@@ -1,5 +1,5 @@
 import { AnyAction } from 'redux'
-import { createFullGrid } from 'utils'
+import { copyGrid, createFullGrid, removeNumbers } from 'utils'
 
 import { IReducer } from './interfaces'
 import * as types from './types'
@@ -8,11 +8,18 @@ const initialState: IReducer = {}
 
 function reducer(state = initialState, action: AnyAction) {
   switch (action.type) {
-    case types.CREATE_GRID:
+    case types.CREATE_GRID: {
+      const solvedGrid = createFullGrid()
+      const gridCopy = copyGrid(solvedGrid)
+      const challengeGrid = removeNumbers(gridCopy)
+      const workingGrid = copyGrid(challengeGrid)
       return {
         ...state,
-        grid: createFullGrid(),
+        challengeGrid,
+        solvedGrid,
+        workingGrid,
       }
+    }
 
     case types.SELECT_BLOCK:
       return {
---
+++ src/utils/get-random-index/index.ts
@@ -0,0 +1,8 @@
+/**
+ * @returns random sudoku grid index in the 0-8 range
+ */
+function getRandomIndex() {
+  return Math.floor(Math.random() * 9)
+}
+
+export default getRandomIndex
